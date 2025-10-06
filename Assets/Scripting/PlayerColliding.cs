18572fe08fac90e94053d90def4ae8f0b7008635
Kieran <kieran.le-mouel@etudiant.univ-rennes.fr>
2025-10-06 13:15:08 +0200

    fixed collider naming convention becauseunity hates fun

--- Assets/Scripting/PlayerColliding.cs
+++ Assets/Scripting/PlayerColliding.cs
@@ -8,19 +8,19 @@ public class PlayerColliding : MonoBehaviour
     public Transform head;
     public Transform floorReference;
 
-    CapsuleCollider collider;
+    CapsuleCollider mycollider;
 
     // Start is called before the first frame update
     void Start()
     {
-        collider = GetComponent<CapsuleCollider>();
+        mycollider = GetComponent<CapsuleCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float height = head.position.y - floorReference.position.y;
-        collider.height = height;
+        mycollider.height = height;
         transform.position = head.position - Vector3.up * height / 2;
     }
 }
